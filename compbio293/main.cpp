a9a6947ff2ac26536e38d02dedcf5b1f839c3171
slwu89 <slwu89@berkeley.edu>
2017-08-30 12:24:49 -0700

    init repo

---
+++ compbio293/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  compbio293
+//
+//  Created by Sean Wu on 8/30/17.
+//  Copyright © 2017 Sean Wu. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
